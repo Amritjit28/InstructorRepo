3cb303a6174af80818a4f021750877d1eed05b28
Amritjit28 <145832689+Amritjit28@users.noreply.github.com>
2026-01-21 12:17:06 +0000

    Revise comments and add author to Lab01_Java.java
    Updated comments and added author information.

--- Lab Folders/Lab01/Lab01_Java.java
+++ Lab Folders/Lab01/Lab01_Java.java
@@ -1,5 +1,6 @@
 //Lab01 Java file
 // Sample Java file
+// Edit made by AmritjitVirdi_240042028.
 void testMethod1()
   {
   //First command x = y + z
@@ -21,3 +22,4 @@ void main()
     testMethod1();
     testMethod2();
 }
+
